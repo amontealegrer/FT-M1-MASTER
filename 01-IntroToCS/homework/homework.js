846e8d1008b57ccc62cd8aac088d51d862ab6f4b
Alexander Montealegre Ramirez <amontealegrer@gmail.com>
2022-09-26 16:01:27 -0500

    Homework 01-IntroToCS completed

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,13 +1,26 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  // tu codigo aca
-
+  var n = num.toString();
+  n = n.split("").reverse().join("");
+  var sum = 0;
+  for (var i = 0; i < n.length; i++) {
+    sum += parseInt(n[i]) * 2 ** i;
+  }
+  return sum;
 }
 
 function DecimalABinario(num) {
-  // tu codigo aca
-
+  if (num / 2 === 1) {
+    return 1
+  } else {
+    var binario = "";
+    while (num >= 1) {
+      binario += (num % 2).toString();
+      num = Math.floor(num / 2);
+    }
+  }
+  return binario.split("").reverse().join("");
 }
 
 
